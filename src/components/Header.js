9c28c7c99367a865f54b573d870e5ec3e884d158
Rosiana Natalie <rosiananatalie@yahoo.com>
2020-05-04 22:46:42 +0800

    styling, jsx and back

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { fireEvent } from "@testing-library/react"
+
+function Header(){
+    const firstName = "Rosiana"
+    const lastName = "Natalie"
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+
+    if (hours < 12){
+        timeOfDay = "Morning"
+    } else if (hours >= 12 && hours < 17){
+        timeOfDay = "afternoon"
+    } else {
+        timeOfDay = "night"
+    }
+
+    return(
+        <h1 className ="navbar"> Hi {`${firstName} ${lastName}`}, Good {timeOfDay}  </h1>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import './style.css'
 
-function MyApp(){
-  return(
-    <div>
-      <h1>Rosiana Natalie</h1>
-      <p>I am PhD Student of Singapore Management University</p>
-      <ul>
-        <li>Singapore</li>
-        <li>Japan</li>
-        <li>Taiwan</li>
-      </ul>   
-    </div>   
-  )
-}
-
-ReactDOM.render(
-  <MyApp />
-  ,document.getElementById("root"))
-
-
+ReactDOM.render(<App />, document.getElementById("root"))
