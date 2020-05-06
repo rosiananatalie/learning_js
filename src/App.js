03ab325318402cb2e2000b8282cca6165a248ed2
Rosiana Natalie <rosiananatalie@yahoo.com>
2020-05-06 12:24:58 +0800

    learning state
    1. state using the conditional operation in the render
    2. todo-app : putting the data from json into state format.

--- src/App.js
+++ src/App.js
@@ -5,26 +5,35 @@ import Footer from "./components/Footer"
 import ContactList from "./components/ContactList"
 import todoData from "./todoData.js"
 
-function App(){
-    const todoComponent = todoData.map(todo => <MainContent key = {todo.id} item = {todo}/>)
+
+class App extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            todo : todoData
+        }
+    }
+
+    render() {
+        const todoComponent = todoData.map(item => <MainContent key = {item.id} item = {item}/>) 
+        return (
+            <div>
+               <Header />
+                
+               <h5>This is to practice about mapping</h5>
+                {todoComponent}
     
-    return (
-        <div>
-           <Header />
-
-           <h5>This is to practice about mapping</h5>
-            {todoComponent}
-
-           <h5>This is to practice about putting props in a list</h5>
-            <ContactList 
-                contact = {{name: "abcd", imgUrl: "http://placekitten.com/400/300",
-                phoneNo: "081123445", email: "abc@yahoo.com"
-                }}
-            />
-
-            <Footer />
-        </div>    
-    )
+               <h5>This is to practice about putting props in a list</h5>
+                <ContactList 
+                    contact = {{name: "abcd", imgUrl: "http://placekitten.com/400/300",
+                    phoneNo: "081123445", email: "abc@yahoo.com"
+                    }}
+                />
+    
+                <Footer />
+            </div>    
+        )
+    }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import './style.css'
-//import App from "./App" --> to-do list app
-//import App2 from "./App2" --> This js app function maps, reusable component
-//import App3 from "./App3" --> product app (how to use map function) practice 
-//import App4 from "./App4" --> class-based component practice
-import App5 from "./App5"
+import App from "./App" //--> to-do list app
+//import App2 from "./App2" //--> This js app function maps, reusable component
+//import App3 from "./App3" //--> product app (how to use map function) practice 
+//import App4 from "./App4" //--> class-based component practice
+//import App5 from "./App5" //--> state practice 1.
+//import App6 from "./App6" //--> state practice 2; putting the conditions in the render function.
 
-ReactDOM.render(<App5 />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
