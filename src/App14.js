5b7cdfefdfbab3b25978ba18d95e3465d52813ea
Rosiana Natalie <rosiananatalie@yahoo.com>
2020-05-07 23:45:06 +0800

    learning form - part 1 with additional value attribute and naming on handleChange func

--- src/App14.js
+++ src/App14.js
@@ -13,18 +13,29 @@ class App14 extends React.Component{
     handleChange(event){
         /** function will (1) grab the current value of the input box, 
          * (2) udpate the state to whatever value that we have in the text box. */
+        const {name,value} = event.target /** THIS IS IMPORTANT!!! */
         this.setState({
-            [event.target.name]: event.target.value
-        })/** represents the element */  
-              
+            [name]: value
+        })/** represents the element */       
     }
 
     render(){
         return(
             <form>
-                <input type = "text" name = "firstName" placeholder="First Name" onChange = {this.handleChange}/>
+                <input 
+                    type = "text" 
+                    name = "firstName" 
+                    value = {this.state.firstName}
+                    placeholder="First Name" 
+                    onChange = {this.handleChange}
+                />
                 <br />
-                <input type = "text" name = "lastName" placeholder="Last Name" onChange = {this.handleChange}/>
+                <input 
+                    type = "text" 
+                    name = "lastName" 
+                    value = {this.state.lastName}
+                    placeholder="Last Name" 
+                    onChange = {this.handleChange}/>
                 
                 <h1>{this.state.firstName} {this.state.lastName}</h1>
             </form>
